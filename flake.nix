d13a50d39ee8cabacee1f35a07faf7f73b75844d
Reikimann <cbirkmand@gmail.com>
2025-05-09 00:12:45 +0200

    Batman no parents

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default =
+      import ./shell.nix { inherit pkgs; };
+  };
+}
