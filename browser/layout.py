8dd236a6cb5e1f9673c960481e8998f21fca804d
Reikimann <cbirkmand@gmail.com>
2025-05-13 16:06:12 +0200

    Added font measure cache and centering support

--- browser/layout.py
+++ browser/layout.py
@@ -5,6 +5,7 @@ HSTEP, VSTEP = 13, 18
 SCROLLBAR_WIDTH = 12
 
 font_cache = {}
+measure_cache = {}
 
 def get_font(size, weight, style):
     key = (size, weight, style)
@@ -13,6 +14,14 @@ def get_font(size, weight, style):
         label = tkinter.Label(font=font)
         font_cache[key] = (font, label)
     return font_cache[key][0]
+
+def get_measure(word, size, weight, style):
+    key = (word, size, weight, style)
+    if key not in measure_cache:
+        font = get_font(size, weight, style)
+        measure = font.measure(word)
+        measure_cache[key] = measure
+    return measure_cache[key]
 
 
 class Text:
@@ -24,16 +33,17 @@ class Tag:
         self.tag = tag
 
 class Layout:
-    def __init__(self, tokens, width):
+    def __init__(self, tokens, screen_width):
         self.display_list = []
-        self.line = []
+        self.line = [] # [{ x: , word: , size: , weight: , style: , centering: ,}]
 
         self.cursor_y: float = VSTEP
         self.cursor_x: float = HSTEP
-        self.width: int = width
+        self.screen_width: int = screen_width
 
         self.weight: Literal["normal", "bold"] = "normal"
         self.style: Literal["roman", "italic"] = "roman"
+        self.centering: bool = False
         self.size: int = 12
 
         for tok in tokens:
@@ -43,13 +53,24 @@ class Layout:
 
     def flush(self):
         if not self.line: return
-        metrics = [font.metrics() for _, _, font in self.line]
+
+        fonts = [get_font(entry["size"], entry["weight"], entry["style"]) for entry in self.line]
+        metrics = [font.metrics() for font in fonts]
         max_ascent = max([metric["ascent"] for metric in metrics])
         baseline = self.cursor_y + 1.25 * max_ascent
 
-        for x, word, font in self.line:
-            y = baseline - font.metrics("ascent")
-            self.display_list.append((x, y, word, font))
+        if any([entry["centering"] for entry in self.line]):
+            last_entry = self.line[-1]
+            last_width = get_measure(last_entry["word"], last_entry["size"], last_entry["weight"], last_entry["style"])
+            line_width = self.line[0]["x"] + last_entry["x"] + last_width
+            centering_delta = (self.screen_width - line_width) / 2
+        else:
+            centering_delta = 0
+
+        for i, entry in enumerate(self.line):
+            font = fonts[i]
+            y = baseline - metrics[i]["ascent"]
+            self.display_list.append((entry["x"] + centering_delta, y, entry["word"], font))
 
         max_descent = max([metric["descent"] for metric in metrics])
         self.cursor_y = baseline + 1.25 * max_descent
@@ -57,15 +78,19 @@ class Layout:
         self.line = []
 
     def word(self, word):
-        font = get_font(self.size, self.weight, self.style)
-        w = font.measure(word)
+        width = get_measure(word, self.size, self.weight, self.style)
+        space = get_measure(" ", self.size, self.weight, self.style)
 
-        if self.cursor_x + w > self.width - HSTEP - SCROLLBAR_WIDTH:
+        if self.cursor_x + width < self.screen_width - HSTEP - SCROLLBAR_WIDTH:
+            self.line.append({"x": self.cursor_x,
+                              "word": word,
+                              "size": self.size,
+                              "weight": self.weight,
+                              "style": self.style,
+                              "centering": self.centering})
+            self.cursor_x += width + space
+        else:
             self.flush()
-
-        self.line.append((self.cursor_x, word, font))
-        self.cursor_x += w + font.measure(" ")
-
 
     def token(self, tok):
         if isinstance(tok, str):
@@ -89,8 +114,12 @@ class Layout:
             self.size += 4
         elif tok.tag == "/big":
             self.size -= 4
-        elif tok.tag == "br":
+        elif tok.tag == "br" or tok.tag == "br /":
             self.flush()
         elif tok.tag == "/p":
             self.flush()
             self.cursor_y += VSTEP
+        elif tok.tag == 'h1 class="title"':
+            self.centering = True
+        elif tok.tag == '/h1':
+            self.centering = False
